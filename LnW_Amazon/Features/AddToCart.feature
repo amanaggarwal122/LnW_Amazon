786d5c2b4ec7d8df06ea4bdc2d0d3debc54c75fa
aman-aggarwal <aman.aggarwal@benchmarkcapital.co.uk>
2024-04-17 19:04:05 +0530

    [LNW-002] First Structure

---
+++ LnW_Amazon/Features/AddToCart.feature
@@ -0,0 +1,9 @@
+﻿Feature: AddToCart
+
+Scenario: Find a product and verify its price
+	Given I am on Amazon India Website
+	And I search for product "Monitor"
+	When I click on the first product in result
+	And I click on Add to cart
+	And I verify the price of the item
+	And I verify the subtotal of the cart
---
+++ LnW_Amazon/StepDefinitions/Steps_AddToCart.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LnW_Amazon.StepDefinitions
+{
+    [Binding]
+    internal class Steps_AddToCart
+    {
+        WebDriver driver = new ChromeDriver();
+
+        [Given(@"I am on Amazon India Website")]
+        public void GivenIAmOnAmazonIndiaWebsite()
+        {
+            driver.Navigate().GoToUrl("https://www.amazon.com/");
+        }
+
+
+        [Given(@"I search for product ""([^""]*)""")]
+        public void GivenISearchForProduct(string monitor)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I click on the first product in result")]
+        public void WhenIClickOnTheFirstProductInResult()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I click on Add to cart")]
+        public void WhenIClickOnAddToCart()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I verify the price of the item")]
+        public void WhenIVerifyThePriceOfTheItem()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I verify the subtotal of the cart")]
+        public void WhenIVerifyTheSubtotalOfTheCart()
+        {
+            throw new PendingStepException();
+        }
+
+    }
+}
